9f7ef69ed407b774a0eff70f14cd7112caad300f
Anton Zdanov <azdanov@pm.me>
2019-02-12 18:46:25 +0200

    feat(App): add Router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { useTranslation } from 'react-i18next'
-import { Container } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Container, Loader } from 'semantic-ui-react'
 import { Footer } from './Footer'
-import { Hero } from './Hero'
-import { Main } from './Main'
 import { NavBar } from './NavBar'
+import { NoMatch } from './NoMatch'
+
+const Main = React.lazy(() => import('./Main'))
+const About = React.lazy(() => import('./About'))
 
 export const App: React.FC = (): JSX.Element => {
   const [t] = useTranslation()
@@ -18,9 +21,18 @@ export const App: React.FC = (): JSX.Element => {
             {t('home')} | {t('translations')}
           </title>
         </Helmet>
-        <NavBar />
-        <Hero />
-        <Main />
+        <Router>
+          <>
+            <NavBar />
+            <React.Suspense fallback={<Loader active inline="centered" />}>
+              <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/about" component={About} />
+                <Route component={NoMatch} />
+              </Switch>
+            </React.Suspense>
+          </>
+        </Router>
       </Container>
       <Footer className="footer" />
     </>
