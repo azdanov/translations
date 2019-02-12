5a79b12cce2c02477abf653420f6d269ae79bb23
Anton Zdanov <azdanov@pm.me>
2019-02-13 01:20:01 +0200

    feat(Toast): add button message

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Button, Label, Segment } from 'semantic-ui-react'
 
-export const Toast: React.FC<{ message: string; onConfirm: () => void }> = ({
-  message,
-  onConfirm,
-}): JSX.Element => (
+export const Toast: React.FC<{
+  message: string
+  button?: string
+  onConfirm: () => void
+}> = ({ message, button = 'OK', onConfirm }): JSX.Element => (
   <Segment basic textAlign="center">
     <Button as="div" labelPosition="left" tabIndex="-1" role="alert" className="toast">
       <Label as="p" basic>
         {message}
       </Label>
-      <Button onClick={onConfirm}>OK</Button>
+      <Button onClick={onConfirm}>{button}</Button>
     </Button>
   </Segment>
 )
