4dc29c4692a84598d4135abce9e37dc379b6ef5a
Anton Zdanov <azdanov@pm.me>
2019-02-13 01:23:02 +0200

    style(src): reformat

--- src/components/ServiceWorkerMessage.tsx
+++ src/components/ServiceWorkerMessage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Toast } from './Toast'
 
