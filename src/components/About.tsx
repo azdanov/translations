c320ac9443e8020b627649db79c36fcd458951d1
Anton Zdanov <azdanov@pm.me>
2019-02-15 16:46:20 +0200

    feat(project): add bg-color

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,15 +10,13 @@ export const About: React.FC = (): JSX.Element => {
   useTitle(`${t('about')} | ${t('translations')}`)
 
   return (
-    <div className="ui basic segment">
-      <div className="ui centered grid">
+    <>
+      <div className="ui centered grid attached">
         <div className="eight wide computer fourteen wide mobile nine wide tablet column">
-          <h1 className="ui center aligned header">
+          <h1 className="ui center aligned header attached top">
             <div className="content">{t('about this')}</div>
           </h1>
-        </div>
-        <div className="row">
-          <div className="eight wide computer fourteen wide mobile nine wide tablet column">
+          <div className="ui segment attached bottom bg-white">
             <p>
               <strong>{t('description')}:</strong> {t('about description')}
             </p>
@@ -36,35 +34,31 @@ export const About: React.FC = (): JSX.Element => {
               </a>
             </p>
           </div>
-        </div>
-        <div className="row">
-          <div className="eight wide computer fourteen wide mobile nine wide tablet column">
-            <h2 className="ui large center aligned header">
-              <div className="content">{t('attributions')}</div>
-            </h2>
-            <div className="ui basic center aligned segment">
-              <a
-                rel="noopener noreferrer"
-                target="_blank"
-                className="ui medium image"
-                href="https://glosbe.com/"
-              >
-                <img alt="Glosbe Logo" src={glosbe} />
-              </a>
-              <div className="ui hidden divider" />
-              <a
-                rel="noopener noreferrer"
-                target="_blank"
-                className="ui medium image"
-                href="https://www.wordnik.com/"
-              >
-                <img alt="Wordnik Logo" src={wordnik} />
-              </a>
-            </div>
+          <h2 className="ui large center aligned header attached">
+            <div className="content">{t('attributions')}</div>
+          </h2>
+          <div className="ui basic center aligned segment attached bottom bg-white">
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              className="ui medium image"
+              href="https://glosbe.com/"
+            >
+              <img alt="Glosbe Logo" src={glosbe} />
+            </a>
+            <div className="ui hidden divider" />
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              className="ui medium image"
+              href="https://www.wordnik.com/"
+            >
+              <img alt="Wordnik Logo" src={wordnik} />
+            </a>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
